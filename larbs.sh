ab42b280c9765be15f2ceed91d8ac2a56c63b117
Luciano Nooijen <lucianonooijen@gmail.com>
2019-01-03 15:52:08 +0100

    Update defaults

--- larbs.sh
+++ larbs.sh
@@ -18,8 +18,8 @@ while getopts ":a:r:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/lucianonooijen/voidrice.git"
+[ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/lucianonooijen/LARBS/master/progs.csv"
 [ -z ${aurhelper+x} ] && aurhelper="yay"
 
 ### FUNCTIONS ###
